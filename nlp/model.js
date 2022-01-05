bf1262080eef8cd3443ed36ea200d599dfd6b272
Ahmedkhan22 <ahmed.afridi091.com>
2022-01-05 18:19:04 +0500

    updated

--- nlp/model.js
+++ nlp/model.js
@@ -1,14 +1,14 @@
-const natural=require('natural')
+const natural = require('natural')
 // create a BayesClassifier
 const Categroy = new natural.BayesClassifier();
 // supply a training set of data for two membership: night and day
-Categroy.addDocument('I see starts', 'night');
-Categroy.addDocument('Moon is in the sky', 'night');
-Categroy.addDocument('It is dark', 'night');
+Categroy.addDocument('How can you find an integer coefficient polynomial knowing its values only at a few points (but requiring the coefficients be small)?', 'Education');
+Categroy.addDocument('What impact would a surge of U.S. troops in Afghanistan have on the state of the American military after eight years of continuous war?', ["Politics", "Society"]);
+Categroy.addDocument('What does, "Some people do the same by their religion," mean?', ['Relgion','Society']);
 Categroy.addDocument('Sun is in the sky', 'day');
 Categroy.addDocument('It is bright', 'day');
 Categroy.addDocument('black lives', 'racixam');
 // training
 Categroy.train();
 
-module.exports=Categroy
+module.exports = Categroy
